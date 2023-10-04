367e8915c4a2695e72fadbd821ae90484005d7da
JULIA APARECIDA DA SILVA FONSECA <23111808@CFP127ALUNO.LOCAL>
2023-10-04 17:06:02 -0300

    finalizado

---
+++ exercicio 03/app.js
@@ -0,0 +1,40 @@
+/****************************************************
+ * OBJETIVO: o cálculo de uma tabuada
+ * DATA: 04/10/2023
+ * AUTOR: Julia Fonseca
+ * VERSÃO: 1.0
+ ***************************************************/
+
+function calcularTabuada(tabuadaInicial, tabuadaFinal, contadorInicial, contadorFinal) {
+    const tabuadas = [];
+
+    for (let t = tabuadaInicial; t <= tabuadaFinal; t++) {
+        const tabuada = [];
+
+        for (let n = contadorInicial; n <= contadorFinal; n++) {
+            const resultado = t * n;
+            tabuada.push(`${t} x ${n} = ${resultado}`);
+        }
+
+        tabuadas.push({
+            tabuada: t,
+            resultados: tabuada
+        });
+    }
+
+    return tabuadas;
+}
+
+function exibirTabuadas(tabuadas) {
+    tabuadas.forEach((tabuada) => {
+        console.log(`Tabuada do [${tabuada.tabuada}]`);
+        tabuada.resultados.forEach((resultado) => {
+            console.log(resultado);
+        });
+    });
+}
+
+module.exports = {
+    calcularTabuada,
+    exibirTabuadas
+};
